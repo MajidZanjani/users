fa5fc2a6c4b9a9f5bfb1d17a9ed2c6c4be4bc98d
majidzanjani <majidmmz1972@gmail.com>
2024-12-13 19:20:10 +0400

    testing deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <header className="row-start-1 flex gap-6 flex-wrap items-center justify-center">
-        header
+        header - testing deployment
       </header>
       <main className="row-start-2 items-center sm:items-start">
         <form
